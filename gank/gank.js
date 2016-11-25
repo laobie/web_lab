3cfcc27a71d7182077c1145ed602f6343a253d1d
jaeger <chjie.jaeger@gmail.com>
2016-11-25 11:25:56 +0800

    format files

--- gank/gank.js
+++ gank/gank.js
@@ -5,60 +5,60 @@
 var gankURL = 'http://gank.io/api/day/';
 var vm = new Vue({
 
-    el: '#app',
+  el: '#app',
 
-    data: {
-        branches: ['master', 'dev'],
-        currentBranch: 'master',
-        day: new Date(),
-        dayData: {
-            category: [],
-            results: null
-        }
-    },
-
-    created: function () {
-        this.getDayData()
-    },
-
-    computed: {
-        dateStr: function () {
-            var d = this.day;
-            return d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate()
-        }
-    },
-
-    methods: {
-        getDateFormat: function () {
-            var d = this.day;
-            return d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate()
-        },
-        toLastDay: function () {
-            this.day = new Date(this.day - 24 * 60 * 60 * 1000);
-        },
-        notFuli: function (item) {
-            console.log(item.type);
-            return item.type != '福利';
-        },
-        getDayData: function () {
-            var xhr = new XMLHttpRequest();
-            var self = this;
-            xhr.open('GET', gankURL + this.dateStr);
-            xhr.onload = function () {
-                self.dayData = JSON.parse(xhr.responseText);
-
-            };
-            xhr.send()
-        }
+  data: {
+    branches: ['master', 'dev'],
+    currentBranch: 'master',
+    day: new Date(),
+    dayData: {
+      category: [],
+      results: null
     }
+  },
+
+  created: function () {
+    this.getDayData()
+  },
+
+  computed: {
+    dateStr: function () {
+      var d = this.day;
+      return d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate()
+    }
+  },
+
+  methods: {
+    getDateFormat: function () {
+      var d = this.day;
+      return d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate()
+    },
+    toLastDay: function () {
+      this.day = new Date(this.day - 24 * 60 * 60 * 1000);
+    },
+    notFuli: function (item) {
+      console.log(item.type);
+      return item.type != '福利';
+    },
+    getDayData: function () {
+      var xhr = new XMLHttpRequest();
+      var self = this;
+      xhr.open('GET', gankURL + this.dateStr);
+      xhr.onload = function () {
+        self.dayData = JSON.parse(xhr.responseText);
+
+      };
+      xhr.send()
+    }
+  }
 });
 
 vm.$watch('dayData', function (val) {
-    if (val.category.length === 0) {
-        this.toLastDay();
-    }
+  if (val.category.length === 0) {
+    this.toLastDay();
+  }
 });
 
 vm.$watch('day', function () {
-    this.getDayData();
+  this.getDayData();
 });
